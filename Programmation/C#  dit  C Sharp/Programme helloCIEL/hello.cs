c62bbbcb90bab72445f5dcf4faecedbf87c436ff
ELECMED by med <86838913+profelecmed@users.noreply.github.com>
2025-06-10 16:10:06 +0200

    Update hello.cs

--- Programmation/C#  dit  C Sharp/Programme helloCIEL/hello.cs
+++ Programmation/C#  dit  C Sharp/Programme helloCIEL/hello.cs
@@ -1,9 +1,12 @@
-using System;
-					
+using System; // Importation de l'espace de noms System
+
+// Définition de la classe principale
 public class Program
 {
-	public static void Main()
-	{
-		Console.WriteLine("Hello les CIEL");
-	}
+    // Point d'entrée du programme
+    public static void Main()
+    {
+        // Affichage du message "Hello World" dans la console
+        Console.WriteLine("Hello World");
+    }
 }
