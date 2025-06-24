5201177766ea7b16874e867b2b87d975386d6e1a
ELECMED by med <86838913+profelecmed@users.noreply.github.com>
2025-06-24 17:33:32 +0200

    Update hellociel_x1024.c

--- Programmation/C/hellociel_x1024.c
+++ Programmation/C/hellociel_x1024.c
@@ -2,4 +2,11 @@
 
 Le C en ligne : https://www.mycompiler.io/fr/new/c */
 
+#include <stdio.h> // Nécessaire pour printf
 
+int main() {
+    for (int i = 0; i < 1024; i++) {
+        printf("%d: Hello Ciel !\n", i); // Correct : %d est un "placeholder" pour un entier, et 'i' est l'argument correspondant.
+    }
+    return 0; // Indique que l'exécution s'est déroulée avec succès
+}
