eee729886a00bf1de1900d29ccd75ac741c5f764
ELECMED by med <86838913+profelecmed@users.noreply.github.com>
2025-06-24 17:26:31 +0200

    Update Hellociel.c

--- Programmation/C/Hellociel.c
+++ Programmation/C/Hellociel.c
@@ -1,9 +1,9 @@
-/* Programme en C  HellloCiel */
-
+/* Programme en C  Helllo les Ciel */
+ 
 
 #include <stdio.h>
 
 int main() {
-    printf("Hello Ciel !\n");
+    printf("Hello les Ciel !\n");
     return 0;
 }
